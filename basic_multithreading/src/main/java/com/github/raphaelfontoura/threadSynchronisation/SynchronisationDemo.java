6e0eb61d43bc0bc6ce6139b65d2a14f95c1286d4
Raphael Fontoura <raphael.fontoura@gmail.com>
2025-02-10 09:37:00 -0300

    Add multithreading synchronization example with custom locks

--- basic_multithreading/src/main/java/com/github/raphaelfontoura/threadSynchronisation/SynchronisationDemo.java
+++ basic_multithreading/src/main/java/com/github/raphaelfontoura/threadSynchronisation/SynchronisationDemo.java
@@ -3,17 +3,21 @@ package com.github.raphaelfontoura.threadSynchronisation;
 public class SynchronisationDemo {
 
   private static int counter = 0;
+  private static int counter1 = 0;
+  private static int counter2 = 0;
 
   public static void main(String[] args) {
     Thread one = new Thread(() -> {
       for (int i = 0; i < 10_000; i++) {
         increment();
+        increment1();
       }
     });
 
     Thread two = new Thread(() -> {
       for (int i = 0; i < 10_000; i++) {
         increment();
+        increment2();
       }
     });
 
@@ -28,10 +32,21 @@ public class SynchronisationDemo {
     }
 
     System.out.println("Counter value: " + counter);
+    System.out.println("Counter 1 and 2 value: " + counter1 + " -- " + counter2);
+    
   }
 
   private synchronized static void increment() {
     counter++;
+  }
+
+  // the synchronized make a block to entirely class to be synchronized
+  private synchronized static void increment1() {
+    counter1++;
+  }
+
+  private synchronized static void increment2() {
+    counter2++;
   }
 }
 
