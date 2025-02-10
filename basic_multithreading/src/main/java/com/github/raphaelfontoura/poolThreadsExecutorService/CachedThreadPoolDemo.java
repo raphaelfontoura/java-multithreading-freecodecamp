f182200415dc347f5c9a768250c07aa542b48b15
Raphael Fontoura <raphael.fontoura@gmail.com>
2025-02-10 16:18:04 -0300

    Add CachedThreadPoolDemo example for demonstrating cached thread pool execution

---
+++ basic_multithreading/src/main/java/com/github/raphaelfontoura/poolThreadsExecutorService/CachedThreadPoolDemo.java
@@ -0,0 +1,35 @@
+package com.github.raphaelfontoura.poolThreadsExecutorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPoolDemo {
+
+  public static void main(String[] args) {
+    ExecutorService service = Executors.newCachedThreadPool();
+
+    for (int i = 0; i < 1000; i++) {
+      service.execute(new TaskOne(i));
+    }
+    service.shutdown();
+  }
+}
+
+class TaskOne implements Runnable {
+
+  private final int taskId;
+
+  public TaskOne(int taskId) {
+    this.taskId = taskId;
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Task: " + taskId + " is being executed by " + Thread.currentThread().getName());
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
