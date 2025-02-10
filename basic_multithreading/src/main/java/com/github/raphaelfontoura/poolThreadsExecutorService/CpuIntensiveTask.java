50b33f2a226c5fc01a971a5ec632365f56be6172
Raphael Fontoura <raphael.fontoura@gmail.com>
2025-02-10 16:50:36 -0300

    Add CpuIntensiveTask example for demonstrating fixed thread pool execution with CPU-intensive tasks

---
+++ basic_multithreading/src/main/java/com/github/raphaelfontoura/poolThreadsExecutorService/CpuIntensiveTask.java
@@ -0,0 +1,25 @@
+package com.github.raphaelfontoura.poolThreadsExecutorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CpuIntensiveTask {
+
+  public static void main(String[] args) {
+    int cores = Runtime.getRuntime().availableProcessors();
+    System.out.println("Number of cores: " + cores);
+    ExecutorService service = Executors.newFixedThreadPool(cores);
+
+    for (int i = 0; i < 20; i++) {
+      service.execute(new CpuTask());
+    }
+    service.shutdown();
+  }
+}
+
+class CpuTask implements Runnable {
+  @Override
+  public void run() {
+    System.out.println("Some CPU intensive task is being executed by " + Thread.currentThread().getName());
+  }
+}
