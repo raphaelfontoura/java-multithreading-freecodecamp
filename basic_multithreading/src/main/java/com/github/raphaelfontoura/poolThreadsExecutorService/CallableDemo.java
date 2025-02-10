0e447a4061a11a7b76ffd3a6f9a33022b3d1cda4
Raphael Fontoura <raphael.fontoura@gmail.com>
2025-02-10 17:14:39 -0300

    Enhance CallableDemo to include timeout for result retrieval and add main thread completion message

--- basic_multithreading/src/main/java/com/github/raphaelfontoura/poolThreadsExecutorService/CallableDemo.java
+++ basic_multithreading/src/main/java/com/github/raphaelfontoura/poolThreadsExecutorService/CallableDemo.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class CallableDemo {
 
@@ -11,8 +12,8 @@ public class CallableDemo {
     try (ExecutorService service = Executors.newFixedThreadPool(2)) {
       Future<Integer> result = service.submit(new ReturnValueTask());
 
-      System.out.println(result.get());
-      
+      System.out.println(result.get(5, TimeUnit.SECONDS));
+      System.out.println("Main thread execution completed");
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
@@ -22,6 +23,7 @@ public class CallableDemo {
 class ReturnValueTask implements Callable<Integer> {
   @Override
   public Integer call() throws Exception {
+    Thread.sleep(2000);
     return 12;
   }
 }
